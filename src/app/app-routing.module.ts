86c90f6cf61f55441abf2b58f11e8d1a2e589ea6
Lucas Coutinho <lucaspp1@ucl.br>
2020-12-11 23:39:06 -0300

    inicio projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ListarUsuarioComponent } from './listar-usuario/listar-usuario.component';
+import { CadastrarUsuarioComponent } from './cadastrar-usuario/cadastrar-usuario.component';
+
+const routes: Routes = [
+  { path: '*', component: ListarUsuarioComponent },
+  { path: '', component: ListarUsuarioComponent },
+  { path: 'listar', component: ListarUsuarioComponent },
+  { path: 'cadastrar', component: CadastrarUsuarioComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarUsuarioComponent } from './listar-usuario/listar-usuario.component';
+import { CadastrarUsuarioComponent } from './cadastrar-usuario/cadastrar-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarUsuarioComponent,
+    CadastrarUsuarioComponent
   ],
   imports: [
     BrowserModule,
